44377ceda6a90c13ab4b4d7986790301fd59d039
Surya Shanmugaselvam <amy6zx@virginia.edu>
2024-06-12 22:57:15 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,128 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Hello</h3>
+        {/* <nav class="navbar navbar-expand-sm navbar-custom fixed-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="./logofile.png" alt="Avatar Logo" class="rounded-pill logo"/> 
+                </a>
+
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa-solid fa-house"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <div class="dropdown">
+                            <a class="nav-link" data-bs-toggle="dropdown" href="#">Arangatram</a>
+                            <ul class="dropdown-menu" style="margin-top: 25px;">
+                              <li><a class="dropdown-item" href="#">Invitation</a></li>
+                              <li><a class="dropdown-item" href="#">Brochure</a></li>
+                              <li><a class="dropdown-item" href="#">Livestream</a></li>
+                              <li><hr class="dropdown-divider"/></li>
+                              <li><a class="dropdown-item" href="#">Guru & Sishya</a></li>
+                              <li><a class="dropdown-item" href="#">Orchestra</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Performances</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Mission</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <div id="demo" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+              <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+              <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+            </div>
+
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="CherryBlossomImage.jpg" alt="CherryBlossomImage" class="d-block" style="width:100%"/>
+              </div>
+              <div class="carousel-item">
+                <img src="AbhayaKaram.jpg" alt="AbhayaKaram" class="d-block" style="width:100%"/>
+              </div>
+              <div class="carousel-item">
+                <img src="StageImage.jpg" alt="StageImage" class="d-block" style="width:100%"/>
+              </div>
+            </div>
+
+            <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon"></span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+              <span class="carousel-control-next-icon"></span>
+            </button>
+        </div>
+
+        <div class="card card-alert"></div>
+
+        <h2 class="absolute-title">Shreeya's Bharatanatyam Dance</h2>
+
+        <h4 class="alert absolute-alert1">It's Happening!!!</h4>
+        <hr class="alert absolute-separator"/>
+        <h6 class="alert absolute-alert2"><b>Shreeya's Arangetram</b> &nbsp;<span class="badge bg-success">Sunday, July 21st</span></h6>
+
+        <button type="button" class="btn btn-outline-dark absolute-btn alert"><b>RSVP to event</b></button>
+        <button type="button" class="btn btn-outline-dark absolute-btn2 alert" data-bs-toggle="modal" data-bs-target="#brochure"><b>Brochure</b></button>
+        <button type="button" class="btn btn-outline-dark absolute-btn3 alert"><b>Livestream</b></button>
+
+        <div class="modal fade" id="brochure">
+            <div class="modal-dialog modal-xl">
+                <div class="modal-content">
+
+                    <div class="modal-header">
+                        <h4 class="modal-title">Shreeya's Arangetram Brochure</h4>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                    </div>
+
+                    <div class="modal-body modal-content">
+                        <div id="brochureGallery" class="carousel slide" data-bs-ride="carousel">
+
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#brochureGallery" data-bs-slide-to="0" class="active"></button>
+                                <button type="button" data-bs-target="#brochureGallery" data-bs-slide-to="1"></button>
+                                <button type="button" data-bs-target="#brochureGallery" data-bs-slide-to="2"></button>
+                            </div>
+
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="la.jpg" alt="Los Angeles" class="d-block" style="width:100%"/>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="chicago.jpg" alt="Chicago" class="d-block" style="width:100%"/>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="ny.jpg" alt="New York" class="d-block" style="width:100%"/>
+                                </div>
+                            </div>
+
+                            <button class="carousel-control-prev" type="button" data-bs-target="#brochureGallery" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon"></span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#brochureGallery" data-bs-slide="next">
+                                <span class="carousel-control-next-icon"></span>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div> */}
     </div>
   );
 }
